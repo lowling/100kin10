b830447c2ec9c5df719c3c2a3ca6b8c0eae9a025
bbertucc <blakebertuccelli@gmail.com>
2020-06-05 13:44:37 -0500

    Update Items

--- dist/script.js
+++ dist/script.js
@@ -1,11 +1,43 @@
+// Nav toggler
 function toggleNav() {
     var element = document.getElementsByTagName('body')[0];
     element.classList.toggle('show_nav');
-  }
+}
+
+// lazyloader
+document.addEventListener("DOMContentLoaded", function() {
+    var lazyloadImages = document.querySelectorAll("img");    
+    var lazyloadThrottleTimeout;
+    
+    function lazyload () {
+      if(lazyloadThrottleTimeout) {
+        clearTimeout(lazyloadThrottleTimeout);
+      }    
+      
+      lazyloadThrottleTimeout = setTimeout(function() {
+          var scrollTop = window.pageYOffset;
+          lazyloadImages.forEach(function(img) {
+              if(img.offsetTop < (window.innerHeight + scrollTop)) {
+                img.src = img.dataset.src;
+                img.classList.remove('lazy');
+              }
+          });
+          if(lazyloadImages.length == 0) { 
+            document.removeEventListener("scroll", lazyload);
+            window.removeEventListener("resize", lazyload);
+            window.removeEventListener("orientationChange", lazyload);
+          }
+      }, 20);
+    }
+    
+    document.addEventListener("scroll", lazyload);
+    window.addEventListener("resize", lazyload);
+    window.addEventListener("orientationChange", lazyload);
+  });
 
 $(document).ready(function() {
 
-    //
+    //Navigation
     $('nav a, ul.toc a').click(function(e){
         e.preventDefault();
         let target = $(this).attr('href');
